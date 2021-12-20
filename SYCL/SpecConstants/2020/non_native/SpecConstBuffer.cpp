db376d8170f1a3a1f3882332bea70d366ca8a95e
HabKaffee <nikita.naumov@intel.com>
2021-12-20 16:48:45 +0300

    Add SpecConstBuffer.cpp

---
+++ SYCL/SpecConstants/2020/non_native/SpecConstBuffer.cpp
@@ -0,0 +1,25 @@
+// REQUIRES: opencl-aot, cpu
+
+// RUN: %clangxx -fsycl -fsycl-targets=spir64_x86_64 %S/Inputs/common.cpp -o %t.out \
+// RUN:          -fsycl-dead-args-optimization
+// RUN: %CPU_RUN_PLACEHOLDER %t.out | FileCheck %t
+
+
+  #include <sycl/sycl.hpp>
+  #include <cmath>
+
+  class Kernel1Name;
+
+  const static sycl::specialization_id<int> SpecConst2{42};
+
+  int main() {
+    sycl::queue Q;    Q.submit([&](sycl::handler &CGH) {
+      CGH.set_specialization_constant<SpecConst2>(1);
+      CGH.single_task<class Kernel1Name>([=](sycl::kernel_handler KH) {
+        (void)KH.get_specialization_constant<SpecConst2>();
+      });
+    });
+    Q.wait();
+    return 0;
+  // CHECK: piMemRelease
+  }
